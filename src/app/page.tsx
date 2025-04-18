cdf822a7186f2da7657af049a2dd28ae6eebb52a
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2025-04-19 05:29:37 +0700

    fix(api): update fetch URL to production endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const handleClassify = (e: React.FormEvent) => {
     e.preventDefault();
     const fetchData = async () => {
-      const res = await fetch("http://localhost:8000/predict", {
+      const res = await fetch("https://emotion-backend-5af8.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text }),
